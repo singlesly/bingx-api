d02a8781039929e0eb06763996e84b0f4d8d2feb
ilinikh.a1 <ilinikh.a@n-t.io>
2023-10-11 01:07:41 +0600

    feat: describe account websocket events #14

--- src/bingx-socket/bingx-account-socket-stream.ts
+++ src/bingx-socket/bingx-account-socket-stream.ts
@@ -1,31 +1,32 @@
-import { Subject } from 'rxjs';
+import { ReplaySubject, Subject } from 'rxjs';
 import { AccountInterface } from '@app/bingx/account/account.interface';
 import {
   BingxGenerateListenKeyEndpoint,
   HttpRequestExecutor,
+  pong,
   RequestExecutorInterface,
 } from '@app/bingx';
-import zlib from 'zlib';
 import { webSocket } from 'rxjs/webSocket';
 import { BingxWebsocketDeserializer } from '@app/bingx-socket/bingx-websocket-deserializer';
 import { BingxWebsocketSerializer } from '@app/bingx-socket/bingx-websocket-serializer';
+import { HeartbeatInterface } from '@app/bingx-socket/interfaces/heartbeat.interface';
 
 export class BingxAccountSocketStream {
   public readonly onConnect$ = new Subject();
   public readonly onDisconnect$ = new Subject();
-  public readonly heartbeat$ = new Subject();
+  public readonly heartbeat$ = new ReplaySubject<HeartbeatInterface>(1);
 
   constructor(
     private readonly account: AccountInterface,
     private readonly requestExecutor: RequestExecutorInterface = new HttpRequestExecutor(),
   ) {
-    this.connect(account, requestExecutor);
+    this.connect(this.account, this.requestExecutor);
   }
 
   private async connect(
     account: AccountInterface,
     requestExecutor: RequestExecutorInterface,
-  ) {
+  ): Promise<void> {
     const {
       data: { listenKey },
     } = await requestExecutor.execute(
@@ -34,7 +35,8 @@ export class BingxAccountSocketStream {
 
     const url = new URL('/swap-market', 'wss://open-api-swap.bingx.com');
     url.searchParams.set('listenKey', listenKey);
-    webSocket({
+
+    const socket$ = webSocket({
       ...new BingxWebsocketDeserializer(),
       ...new BingxWebsocketSerializer(),
       url: url.toString(),
@@ -42,5 +44,7 @@ export class BingxAccountSocketStream {
       openObserver: this.onConnect$,
       closeObserver: this.onDisconnect$,
     });
+
+    socket$.pipe(pong(socket$, this.heartbeat$)).subscribe();
   }
 }
---
+++ src/bingx-socket/events/account-websocket-events.ts
@@ -0,0 +1,140 @@
+import { OrderPositionSideEnum, OrderSideEnum } from '@app/bingx';
+
+export enum AccountWebsocketEventType {
+  LISTEN_KEY_EXPIRED = 'listenKeyExpired',
+  ACCOUNT_UPDATE = 'ACCOUNT_UPDATE',
+  ORDER_TRADE_UPDATE = 'ORDER_TRADE_UPDATE',
+  ACCOUNT_CONFIG_UPDATE = 'ACCOUNT_CONFIG_UPDATE',
+}
+
+export enum AccountUpdateEventLaunchReason {
+  DEPOSIT = 'DEPOSIT',
+  WITHDRAW = 'WITHDRAW',
+  ORDER = 'ORDER',
+  FUNDING_FEE = 'FUNDING_FEE',
+}
+
+export enum WebSocketOrderType {
+  MARKET = 'MARKET',
+  LIMIT = 'LIMIT',
+  STOP = 'STOP',
+  TAKE_PROFIT = 'TAKE_PROFIT',
+  LIQUIDATION = 'LIQUIDATION',
+}
+
+export enum WebSocketOrderExecutionType {
+  NEW = 'NEW',
+  CANCELED = 'CANCELED',
+  CALCULATED = 'CALCULATED',
+  EXPIRED = 'EXPIRED',
+  TRADE = 'TRADE',
+}
+
+export enum WebSocketOrderStatus {
+  NEW = 'NEW',
+  PARTIALLY_FILLED = 'PARTIALLY_FILLED',
+  FILLED = 'FILLED',
+  CANCELED = 'CANCELED',
+  EXPIRED = 'EXPIRED',
+}
+
+export enum WebSocketTriggerPriceType {
+  MARK_PRICE = 'MARK_PRICE',
+  CONTRACT_PRICE = 'CONTRACT_PRICE',
+  INDEX_PRICE = 'INDEX_PRICE',
+}
+
+export type EventTimeInMilliseconds = number;
+
+/**
+ * Relates account events
+ */
+export type AssetName = string;
+export type WalletBalance = number;
+export type WalletBalanceExcludingIsolatedMargin = number;
+export type WalletBalanceChangeAmount = number;
+export type TradingPair = `${string}-${string}`;
+export type Position = number;
+export type EntryPrice = number;
+export type UnrealizedProfitAndLossPosition = number;
+export type MarginMode = string;
+export type IsolatedPositionMargin = number;
+
+/**
+ * Relates to order update events
+ */
+export type ClientCustomOrderId = string;
+export type OrderId = string;
+export type Quantity = number;
+export type Price = number;
+export type AveragePrice = number;
+export type HandlingFee = number;
+export type TransactionTime = number;
+export type TransactionAchievedProfitAndLoss = number;
+export type OrderFilledAccumulatedQuantity = number;
+export type FeeAssetType = string;
+
+export interface AccountBalanceInformation {
+  a: AssetName;
+  wb: WalletBalance;
+  cw: WalletBalanceExcludingIsolatedMargin;
+  bc: WalletBalanceChangeAmount;
+}
+
+export interface TradeInfo {
+  s: TradingPair;
+  pa: Position;
+  ep: EntryPrice;
+  up: UnrealizedProfitAndLossPosition;
+  mt: MarginMode;
+  iw: IsolatedPositionMargin;
+  ps: OrderPositionSideEnum;
+}
+
+export interface AccountWebSocketOrder {
+  s: TradingPair;
+  c: ClientCustomOrderId;
+  i: OrderId;
+  S: OrderSideEnum;
+  o: WebSocketOrderType;
+  q: Quantity;
+  p: Price;
+  ap: AveragePrice;
+  x: WebSocketOrderExecutionType;
+  X: WebSocketOrderStatus;
+  N: FeeAssetType;
+  n: HandlingFee;
+  T: TransactionTime;
+  wt: WebSocketTriggerPriceType;
+  ps: OrderPositionSideEnum;
+  rp: TransactionAchievedProfitAndLoss;
+  z: OrderFilledAccumulatedQuantity;
+}
+
+export interface AccountUpdateEvent {
+  m: AccountUpdateEventLaunchReason;
+  B: AccountBalanceInformation[];
+  P: TradeInfo[];
+}
+
+export interface AccountWebSocketEvent {
+  e: AccountWebsocketEventType;
+  E: EventTimeInMilliseconds;
+}
+
+export interface ListenKeyExpiredEvent extends AccountWebSocketEvent {
+  e: AccountWebsocketEventType.LISTEN_KEY_EXPIRED;
+  listenKey: string;
+}
+
+export interface AccountBalanceAndPositionPushEvent
+  extends AccountWebSocketEvent {
+  e: AccountWebsocketEventType.ACCOUNT_UPDATE;
+  T: EventTimeInMilliseconds;
+  a: AccountUpdateEvent;
+}
+
+export interface AccountOrderUpdatePushEvent extends AccountWebSocketEvent {
+  e: AccountWebsocketEventType.ORDER_TRADE_UPDATE;
+  E: EventTimeInMilliseconds;
+}
