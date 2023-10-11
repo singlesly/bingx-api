2b76982bf8737d118ea5fe9b412584791e7b38a8
ilinikh.a1 <ilinikh.a@n-t.io>
2023-10-11 06:33:03 +0600

    feat: remove unused import #14

--- src/bingx-client/services/listen-key.service.ts
+++ src/bingx-client/services/listen-key.service.ts
@@ -2,7 +2,6 @@ import { AccountInterface } from '@app/bingx/account/account.interface';
 import { BingxGenerateListenKeyEndpoint } from '@app/bingx/endpoints/bingx-generate-listen-key-endpoint';
 import { BingxGenerateListenKeyResponse } from '@app/bingx/endpoints/bingx-generate-listen-key-response';
 import { RequestExecutorInterface } from '@app/bingx/request-executor/request-executor.interface';
-import { BingxResponseInterface } from '@app/bingx/endpoints/bingx-response.interface';
 
 export class ListenKeyService {
   constructor(private readonly requestExecutor: RequestExecutorInterface) {}
