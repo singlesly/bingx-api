8e89b6938e3a168aea2ab4a1657c640e0c6967a8
ilinikh.a1 <ilinikh.a@n-t.io>
2023-10-13 04:43:22 +0600

    feat: implement switch margin mode #40

--- src/bingx/account/default-signature-parameters.ts
+++ src/bingx/account/default-signature-parameters.ts
@@ -5,7 +5,7 @@ export class DefaultSignatureParameters
 {
   constructor(
     private readonly parameters:
-      | Record<string, string>
+      | Record<string, string | number>
       | string
       | URLSearchParams = {},
     private readonly overrideTimestamp: number = Date.now(),
