6cb367e5bbccaccefdb4daa8c6bd6e4d7ea1707c
ilinikh.a1 <ilinikh.a@n-t.io>
2023-10-13 03:21:18 +0600

    feat: implement switch margin mode #39

---
+++ src/bingx/endpoints/bingx-switch-margin-mode-endpoint.ts
@@ -0,0 +1,44 @@
+import {
+  AccountInterface,
+  DefaultSignatureParameters,
+  Endpoint,
+  EndpointInterface,
+  SignatureParametersInterface,
+} from '@app/bingx';
+
+export interface SwitchMarginModeResponse {
+  code: number;
+  msg: string;
+}
+
+export type MarginType = 'ISOLATED' | 'CROSSED';
+
+export class BingxSwitchMarginModeEndpoint
+  extends Endpoint
+  implements EndpointInterface<SwitchMarginModeResponse>
+{
+  public constructor(
+    private readonly symbol: string,
+    private readonly marginType: MarginType,
+    account: AccountInterface,
+  ) {
+    super(account);
+  }
+
+  method(): 'get' | 'post' | 'put' | 'patch' | 'delete' {
+    return 'post';
+  }
+
+  parameters(): SignatureParametersInterface {
+    return new DefaultSignatureParameters({
+      symbol: this.symbol,
+      marginType: this.marginType,
+    });
+  }
+
+  path(): string {
+    return '/openApi/swap/v2/trade/marginType';
+  }
+
+  readonly t!: SwitchMarginModeResponse;
+}
